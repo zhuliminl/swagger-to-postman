5e2796054494e9420429ca996aa3d570117a2fd0
zhuliminl <zhuliminl@gmail.com>
2022-12-07 14:03:44 +0800

    readme

---
+++ secrets.js
@@ -0,0 +1,7 @@
+// Keep your API key secret by renaming this file `secrets.js` and adding your API key.
+// This file will not be checked into git once you've renamed it to `secrets.js`.
+// WARNING: never check in your API key to git!!
+
+module.exports = {
+    "key": "PMAK-638ed8843412bd0b1bcdaa4a-11307a75ec8e3b07853ab940cc129ad4bc"
+};
